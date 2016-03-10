3df969a3223e96060809b04fdf38b4ca2381916b
Meghana <meghanashastri07@gmail.com>
2016-03-10 19:26:37 +0530

    tab layout added

--- app/src/main/java/com/example/admin/medicare/MainActivity.java
+++ app/src/main/java/com/example/admin/medicare/MainActivity.java
@@ -3,11 +3,9 @@ package com.example.admin.medicare;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,36 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initialiseTabLayout();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * Method to initialise the Tab Layout and set Text
+     */
+    private void initialiseTabLayout() {
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText(getResources()
+                .getText(R.string.tab_category)));
+        tabLayout.addTab(tabLayout.newTab().setText(getResources()
+                .getText(R.string.tab_generic)));
+        tabLayout.addTab(tabLayout.newTab().setText(getResources()
+                .getText(R.string.tab_brand)));
+        tabLayout.setTabTextColors(getResources().getColor(R.color.color_grey), getResources()
+                .getColor(R.color.colorPrimary));
     }
 }
