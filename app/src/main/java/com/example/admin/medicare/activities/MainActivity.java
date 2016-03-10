73c97652741fe35717b5fe724c03a93f6200d41c
Meghana <meghanashastri07@gmail.com>
2016-03-10 23:11:37 +0530

    Refactored code

--- app/src/main/java/com/example/admin/medicare/activities/MainActivity.java
+++ app/src/main/java/com/example/admin/medicare/activities/MainActivity.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.example.admin.medicare.R;
 import com.example.admin.medicare.fragments.MedicareFragment;
+import com.example.admin.medicare.utilities.DatabaseHelper;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseHelper databaseHelper = null;
+    private List<String> categoryItems, genericItems,brandItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initialiseTabLayout();
+        initialiseDatabaseHelperClass();
+        categoryItems = databaseHelper.getEntireCateg();
+        brandItems = databaseHelper.getEntireBrand();
+        genericItems = databaseHelper.getEntireGeneric();
+        changeFragment(MedicareFragment.class.getName(),0);
+    }
+    private void initialiseDatabaseHelperClass(){
+        try {
+            databaseHelper=new DatabaseHelper(this,this.getFilesDir().getAbsolutePath());
+            databaseHelper.prepareDatabase();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            Log.e(getClass().getName(), e1.getMessage());
+        }
     }
 
     /**
@@ -84,5 +105,17 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public List<String> getCategoryItems(){
+        return categoryItems;
+    }
+
+    public List<String> getGenericItems(){
+        return genericItems;
+    }
+
+    public List<String> getBrandItems(){
+        return brandItems;
     }
 }
--- app/src/main/java/com/example/admin/medicare/fragments/MedicareFragment.java
+++ app/src/main/java/com/example/admin/medicare/fragments/MedicareFragment.java
@@ -11,28 +11,24 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.admin.medicare.R;
+import com.example.admin.medicare.activities.MainActivity;
 import com.example.admin.medicare.adapters.ListViewAdapter;
-import com.example.admin.medicare.utilities.DatabaseHelper;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by Admin on 10-03-2016.
  */
-public class MedicareFragment extends Fragment{
+public class MedicareFragment extends Fragment {
     private static final String FRAGMENT_NUMBER = "param1";
     private int mFragmentNumber;
     private View rootView;
-    private ListView listView;
-    private List<String> items;
     private Context mContext;
-    private DatabaseHelper databaseHelper=null;
-    public MedicareFragment(){
 
+    public MedicareFragment() {
     }
 
-    public static MedicareFragment newInstance(int fragmentNumber){
+    public static MedicareFragment newInstance(int fragmentNumber) {
         MedicareFragment fragment = new MedicareFragment();
         Bundle args = new Bundle();
         args.putInt(FRAGMENT_NUMBER, fragmentNumber);
@@ -43,7 +39,7 @@ public class MedicareFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initialiseDatabaseHelperClass();
+        mContext = getActivity().getApplicationContext();
         if (getArguments() != null) {
             mFragmentNumber = getArguments().getInt(FRAGMENT_NUMBER);
         }
@@ -52,44 +48,38 @@ public class MedicareFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_medicare,container,false);
+        rootView = inflater.inflate(R.layout.fragment_medicare, container, false);
         setAdapterToFragment();
         return rootView;
     }
 
-    private void initialiseDatabaseHelperClass(){
-        try {
-            databaseHelper=new DatabaseHelper(mContext,mContext.getFilesDir().getAbsolutePath());
-            databaseHelper.prepareDatabase();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            Log.e(getClass().getName(), e1.getMessage());
-        }
-    }
 
-    private void setAdapterToFragment(){
-        switch (mFragmentNumber){
+    private void setAdapterToFragment() {
+        switch (mFragmentNumber) {
             case 0:
-                List<String> categoryItems = databaseHelper.getEntireCateg();
-                setAdapterToListView(categoryItems);
+                List<String> items = ((MainActivity) getActivity()).getCategoryItems();
+                Log.d("category", items.toString());
+                setAdapterToListView(items);
                 break;
             case 1:
-                List<String> brandItems = databaseHelper.getEntireBrand();
-                for (int i=0;i<brandItems.size();i++){
-                    brandItems.set(i, brandItems.get(i).replaceAll("�", ","));
+                items = ((MainActivity) getActivity()).getBrandItems();
+                Log.d("brand", items.toString());
+                for (int i = 0; i < items.size(); i++) {
+                    items.set(i, items.get(i).replaceAll("�", ","));
                 }
-                setAdapterToListView(brandItems);
+                setAdapterToListView(items);
                 break;
             case 2:
-                List<String> genericItems = databaseHelper.getEntireGeneric();
-                setAdapterToListView(genericItems);
+                items = ((MainActivity) getActivity()).getGenericItems();
+                Log.d("generic", items.toString());
+                setAdapterToListView(items);
                 break;
         }
     }
 
-    private void setAdapterToListView(List<String> items){
-        listView = (ListView) rootView.findViewById(R.id.listView);
-        ListViewAdapter listViewAdapter = new ListViewAdapter(mContext,items);
+    private void setAdapterToListView(List<String> items) {
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+        ListViewAdapter listViewAdapter = new ListViewAdapter(mContext, items);
         listView.setAdapter(listViewAdapter);
     }
 }
